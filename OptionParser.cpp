2c6257bf87c2de0cca615ca6a11f9f77e6ce7454
Pontus Ahlqvist <pontus.ahlqvist@gmail.com>
2016-07-05 21:55:09 -0700

    Created OptionParser class that will help parse command line flags

---
+++ OptionParser.cpp
@@ -0,0 +1,56 @@
+//
+//  OptionParser.cpp
+//  NeuralNetwork
+//
+//  Created by Pontus Ahlqvist on 7/5/16.
+//  Copyright (c) 2016 PontusAhlqvist. All rights reserved.
+//
+
+#include "OptionParser.h"
+
+OptionParser::OptionParser(){
+    keys.push_back("-h");
+    descriptions["-h"] = "Display this help message and exit";
+}
+
+void OptionParser::add_option(std::string key, std::string description){
+    keys.push_back(key);
+    descriptions[key] = description;
+}
+
+void OptionParser::printDescriptions(){
+    std::cout << "Usage: ./[program] [options]\n";
+    for(int i = 0; i < keys.size(); i++){
+        std::cout << keys[i] << "\t" << descriptions[keys[i]] << "\n";
+    }
+    std::cout << std::endl;
+}
+
+//return type is true if parse goes ok and false if '-h' is encountered or any other issue is encountered
+bool OptionParser::parse_options(int argc, const char **argv){
+    for(int i = 1; i < argc; i = i+2){ //skip first input
+        std::string key = argv[i];
+        if(key == "-h"){
+            printDescriptions();
+            return false;
+        }
+
+        std::string value = argv[i+1];
+        optionValues[key] = value;
+    }
+    return true;
+}
+
+std::vector<std::string> OptionParser::getKeys(){
+    return keys;
+}
+
+std::string OptionParser::getValue(std::string key){
+    std::map<std::string,std::string>::iterator it = optionValues.find(key);
+    if(it != optionValues.end()){
+        return optionValues[key];
+    } else{
+        return std::string("");
+    }
+}
+
---
+++ OptionParser.h
@@ -0,0 +1,32 @@
+//
+//  OptionParser.h
+//  NeuralNetwork
+//
+//  Created by Pontus Ahlqvist on 7/5/16.
+//  Copyright (c) 2016 PontusAhlqvist. All rights reserved.
+//
+
+#ifndef __NeuralNetwork__OptionParser__
+#define __NeuralNetwork__OptionParser__
+
+#include <map>
+#include <vector>
+#include <string>
+#include <iostream>
+
+class OptionParser{
+protected:
+    std::vector<std::string> keys;
+    std::map<std::string, std::string> optionValues; //from key to value
+    std::map<std::string, std::string> descriptions;
+    void printDescriptions();
+public:
+    OptionParser();
+    void add_option(std::string key, std::string description);
+    bool parse_options(int argc, const char** argv);
+    //Three different methods to return value for various
+    std::vector<std::string> getKeys();
+    std::string getValue(std::string key);
+};
+
+#endif /* defined(__NeuralNetwork__OptionParser__) */
